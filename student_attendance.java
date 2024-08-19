5f1fe24e0c3eb83aca992ec5d3f86fcd9a61eed3
Enoch Jason <enochjason@Enochs-MacBook-Air.local>
2024-08-19 19:23:38 +0530

    Edited Settings

---
+++ student_attendance.java
@@ -0,0 +1,8 @@
+public class student_attendance{
+    public static void main(String[] args) {
+        // input -> [101,0,105,10,0,7,0]
+        // output -> [101,105,10,7,0,0,0]
+        int array1[] = {101,0,105,0,10,7,0};
+        System.out.println(array1);
+    }
+}
