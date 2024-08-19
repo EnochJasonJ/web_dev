bd4425d10e1bcbf2521d4623b05ef9f82b5f2f3d
Enoch Jason <enochjason@Enochs-MacBook-Air.local>
2024-08-19 20:17:58 +0530

    Added the class.

--- student_attendance.java
+++ student_attendance.java
@@ -3,9 +3,14 @@ public class student_attendance{
         // input -> [101,0,105,10,0,7,0]
         // output -> [101,105,10,7,0,0,0]
         int array1[] = {101,0,105,0,10,7,0};
-        for(int i = 0; i < array1.length; i++){
-            System.out.println(array1[i]);
+        for (int i = 0;i < array1.length; i++){
+            for(int j = 0; j < array1.length; j++){
+                
+            }
         }
-        // System.out.println(array1);
+        for(int x = 0; x < array1.length; x++){
+            System.out.println(array1[x]);
+        }
+        
     }
 }
