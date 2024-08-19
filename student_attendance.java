048819d05d4be83bfc27b57d8e939ee1569352a4
Enoch Jason <enochjason@Enochs-MacBook-Air.local>
2024-08-19 19:47:03 +0530

    Updated student_attendance.java.

--- student_attendance.java
+++ student_attendance.java
@@ -3,6 +3,9 @@ public class student_attendance{
         // input -> [101,0,105,10,0,7,0]
         // output -> [101,105,10,7,0,0,0]
         int array1[] = {101,0,105,0,10,7,0};
-        System.out.println(array1);
+        for(int i = 0; i < array1.length; i++){
+            System.out.println(array1[i]);
+        }
+        // System.out.println(array1);
     }
 }
